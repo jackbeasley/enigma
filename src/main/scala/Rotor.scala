1b2e4826cc4fb88c373dae9d903ffad4d2cb40ef
Jack Beasley <jack.beasley@gmail.com>
2014-11-30 20:15:31 -0800

    Add inital Rotor support

---
+++ src/main/scala/Rotor.scala
@@ -0,0 +1,32 @@
+class Rotor (stepAt: Char, ciphers: Array[(Char, Char)], stepCallback: () => Unit) {
+
+	val stepAtNum = stepAt.toUpper.toInt - 65
+	val forwardCipher: Map[Char,Char] = ciphers.toMap
+	val backwardCipher: Map[Char,Char] = ciphers.map(t => (t._2, t._1)).toMap
+	/*
+	 * Rotor position
+	 * For example, if 0, a -> a; if 1, a-> b
+	 * Smallest value is 0, for machine this would be 1
+	 * Because of this, the max number is 25, not 26
+	 */
+	var offset: Int = 0
+
+	def encodeForward(input: Char): Char = {
+		return forwardCipher(applyOffset(input))
+	}
+
+	def endodeBackward(input: Char): Char = backwardCipher(applyOffset(input))
+
+	def applyOffset(letter: Char) : Char = (letter.toInt + offset).toChar
+
+	def turnRotor = {
+		if(offset < 25){
+			offset += 1
+		} else {
+			offset = 0
+		}
+		if(offset == stepAtNum){
+			stepCallback
+		}
+	}
+}
---
+++ src/main/scala/RotorI.scala
@@ -0,0 +1,40 @@
+class RotorI(stepCallback: () => Unit) 
+	extends Rotor(
+		//Steps at 'R'
+		'R',
+		//Value pairs
+		Array(
+			('A','E'),
+			('B','K'),
+			('C','M'),
+			('D','F'),
+			('E','L'),
+			('F','G'),
+			('G','D'),
+			('H','Q'),
+			('I','V'),
+			('J','Z'),
+			('K','N'),
+			('L','T'),
+			('M','O'),
+			('N','W'),
+			('O','Y'),
+			('P','H'),
+			('Q','X'),
+			('R','U'),
+			('S','S'),
+			('T','P'),
+			('U','A'),
+			('V','I'),
+			('W','B'),
+			('X','R'),
+			('Y','C'),
+			('Z','J')
+		),
+		//Callback passed to main constructor
+		stepCallback) {
+
+		//Rotor type: I -> 1, II -> 2, III - > 3...
+		def rotorType = 1
+
+}
