c4bc95a84c399d9aa2697ab15a4931d1c1c9d8b3
Jack Beasley <jack.beasley@gmail.com>
2014-11-30 22:01:51 -0800

    Add rotor tests in main runner

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,15 @@
+package com.jackbeasley.enigma
+
 object Main {
-	def main(args: Array[String]) = println("Hello")
+	def main(args: Array[String]) {
+		val rotorI = new RotorI(() => Unit)
+		rotorI.setOffsetPosition(0);
+		println(rotorI.encodeForward('A'))
+		rotorI.turnRotor
+		println(rotorI.encodeForward('A'))
+		rotorI.setOffsetPosition(25);
+		println(rotorI.encodeForward('A'))
+		rotorI.turnRotor
+		println(rotorI.encodeForward('A'))
+	}
 }
