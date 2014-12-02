d984f68895b0c1228a5d7b810757e1a7eabcd4f7
Jack Beasley <jack.beasley@gmail.com>
2014-12-01 20:32:21 -0800

    Remove test code from main singleton object

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,5 @@
 package com.jackbeasley.enigma
 
 object Main {
-	def main(args: Array[String]) {
-		val rotorI = new RotorI(() => Unit)
-		rotorI.setOffsetPosition(0);
-		println(rotorI.encodeForward('A'))
-		rotorI.turnRotor
-		println(rotorI.encodeForward('A'))
-		rotorI.setOffsetPosition(25);
-		println(rotorI.encodeForward('A'))
-		rotorI.turnRotor
-		println(rotorI.encodeForward('A'))
-	}
+
 }
